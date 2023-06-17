be8db866c6f26fa60b3993b5f8e33e8dcfa1e7a5
adrianrojas2019 <rojasadrian@hotmail.com>
2023-06-16 22:59:24 -0600

    draft version

---
+++ index.js
@@ -0,0 +1,80 @@
+import {ApolloServer, gql} from 'apollo-server'
+
+const students = [
+    {
+        name: "Adrian",
+        phone: "50688288542",
+        age: "20",
+        birthday: "3/8/2002",
+        city : "Heredia",
+        country: "Costa Rica"
+    },
+    {
+        name: "Cinthia",
+        phone: "50688418345",
+        age: "20",
+        birthday: "10/8/1982",
+        city : "Heredia",
+        country: "Costa Rica"
+    },
+    {
+        name: "John",
+        phone: "56188288542",
+        age: "40",
+        birthday: "3/8/1982",
+        city : "West Palm Beach",
+        country: "United States"
+    },
+    {
+        name: "Tom",
+        phone: "561842612",
+        age: "19",
+        birthday: "3/8/2002",
+        city : "Cancun",
+        country: "Mexico"
+    },
+    {
+        name: "Josh",
+        phone: "588288542",
+        age: "20",
+        birthday: "3/8/2002",
+        city : "Paris",
+        country: "France"
+    },
+]
+
+const typeDefs = gql`
+    type Student {
+        name: String!
+        phone: String
+        age: String!
+        city: String
+        country: String
+    }
+
+    type Query {
+        studentCount: Int!
+        allStudents: [Student]!
+        findStudent(name: String!): Student
+    }
+`
+
+const resolvers = {
+    Query: {
+        studentCount: () => students.length,
+        allStudents: () => students,
+        findStudent: (root, args) => {
+            const {name} = args
+            return students.find(student => student.name === name)
+        }
+    }
+}
+
+const server = new ApolloServer({
+    typeDefs,
+    resolvers
+})
+
+server.listen().then(({url}) => {
+    console.log(`Server ready at ${url}`)
+})
